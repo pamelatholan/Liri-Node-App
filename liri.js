64b1ed7eee848366ad9ca6e21b664a0514402d37
Pam Tholan <pamelatholan@yahoo.com>
2019-07-02 14:48:36 -0400

    all calls working in terminal and logging

--- liri.js
+++ liri.js
@@ -3,6 +3,9 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var request = require("request");
 var fs = require("fs");
+var moment = require('moment');
+moment().format();
+var axios = require('axios');
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 //vars to capture user inputs.
@@ -106,7 +109,7 @@ function showMovieInfo(inputParameter){
         console.log("It's on Netflix!");
         fs.appendFileSync("log.txt", "It's on Netflix!\n");
     }
-    var queryUrl = "http://www.omdbapi.com/?t=" + inputParameter + "&y=&plot=short&apikey=b3c0b435";
+    var queryUrl = "http://www.omdbapi.com/?t=" + inputParameter + "&y=&plot=short&apikey=trilogy";
     request(queryUrl, function(error, response, body) {
     // If the request is successful
     if (!error && response.statusCode === 200) {
